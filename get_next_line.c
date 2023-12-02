bad9153c70b07d5e297594eb0982173d1feb7af1
Ubuntu <ubuntu@ip-172-31-45-112.us-east-2.compute.internal>
2023-12-02 23:30:26 +0000

    testing

--- get_next_line.c
+++ get_next_line.c
@@ -10,112 +10,6 @@
 # define BUFFER_SIZE 10
 #endif
 
-// char *get_next_line(int fd)
-// {
-//     char *line;
-//     static char *leftover = NULL;
-//     int i = 0;
-//     int j = 0;
-//     int read_chars;
-//     char chunk[BUFFER_SIZE + 1];
-
-//     if (fd < 0)
-//         return (NULL);
-
-//     line = calloc(BUFFER_SIZE + 1, sizeof(char));
-//     if (line == NULL)
-//         return (NULL);
-
-//     // Process leftover from previous reads
-//     if (leftover != NULL)
-//     {
-//         while (leftover[j] != '\n' && leftover[j] != '\0')
-//         {
-//             line[i++] = leftover[j++];
-//         }
-
-//         if (leftover[j] == '\n')
-//         {
-//             char *temp = strdup(leftover + j + 1);
-//             free(leftover);
-//             leftover = temp;
-//             line[i++] = '\n';
-//             line[i] = '\0';
-//             return (line);
-//         }
-
-//         free(leftover);
-//         leftover = NULL;
-//     }
-
-//     // Read new data
-//     read_chars = read(fd, chunk, BUFFER_SIZE);
-//     while (read_chars > 0)
-//     {
-//         chunk[read_chars] = '\0';
-//         if (!strchr(chunk, '\n'))
-//         {
-//             char *temp = realloc(line, i + read_chars + 1);
-//             if (temp == NULL) {
-//                 free(line);
-//                 return (NULL);
-//             }
-//             line = temp;
-//             strcat(line, chunk);
-//             i += strlen(chunk);
-//         }
-//         else
-//         {
-//             while (j < read_chars && chunk[j] != '\n')
-//             {
-//                 line[i] = chunk[j];
-//                 i++;
-//                 j++;
-//             }
-
-//             if (leftover != NULL) {
-//                 free(leftover);
-//             }
-//             leftover = strdup(chunk + j + 1);
-//             line[i++] = '\n';
-//             line[i] = '\0';
-//             return (line);
-//         }
-//         read_chars = read(fd, chunk, BUFFER_SIZE);
-//     }
-
-//     // Handle end of file
-//     if (i > 0)
-//     {
-//         line[i] = '\0';
-//         return (line);
-//     }
-//     else
-//     {
-//         free(line);
-//         return (NULL);
-//     }
-// }
-
-// int main(void)
-// {
-//     int fd;
-
-//     fd = open("text.txt", O_RDONLY);
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-//     printf("%s", get_next_line(fd));
-
-//     close(fd);
-//     return (0);
-// }
-
 char *get_next_line(int fd)
 {
     char *line;
@@ -155,7 +49,7 @@ char *get_next_line(int fd)
     }
 
     // Read new data
-    read_chars = read(fd, line + i, BUFFER_SIZE);
+    read_chars = read(fd, line + i, BUFFER_SIZE - i);
     while (read_chars > 0)
     {
         i += read_chars;
